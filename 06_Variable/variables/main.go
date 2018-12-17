3b647ecbfbf4d54c836354e8f04f4568635ce710
Arun Kr. Khattri <arunkhattri@outlook.com>
2018-12-17 18:11:56 +0530

    Added variable initialisation

---
+++ 06_Variable/variables/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var speed int
+	// speed will be initialised with 0 value
+	var ratio float64
+	var off bool
+	var msg string
+	fmt.Println(speed)
+	fmt.Println(ratio)
+	fmt.Println(off)
+	fmt.Println(msg)
+	fmt.Printf("%q\n", msg)
+}
